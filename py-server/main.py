e8475db9a2f4e1c26610f278b4818b637d87ad91
Muteen Kundangar <kundangarmuteen@gmail.com>
2026-04-15 23:12:51 +0530

    Payment route init

--- py-server/main.py
+++ py-server/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, HTTPException
+from pydantic import BaseModel
 import db
 
 app = FastAPI()
@@ -6,3 +7,19 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "API running"}
+
+
+class PayRequestPayload(BaseModel):
+    paid_to: str
+    amount: float
+
+@app.post("/pay")
+async def pay_route(request: Request, payload: PayRequestPayload):
+    try:
+        idempotency_key = request.headers.get('idempotency-key')
+        if not idempotency_key:
+            return HTTPException(400, "Idempotency key required !")
+        return {"message": f"{idempotency_key}"}
+    except Exception as e:
+        return HTTPException(500, f"An error occurred while processing: {str(e)}")
+    
