1b6452380b39fb79ea78737c71bec339c0a69bc7
Muteen Kundangar <kundangarmuteen@gmail.com>
2026-04-16 02:15:10 +0530

    Added a base idempotency to the pay api endpoint

--- py-server/main.py
+++ py-server/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Request, HTTPException
 from pydantic import BaseModel
 from sqlalchemy.exc import IntegrityError
+import time, asyncio
 
 import db as db_session
 
@@ -17,12 +18,11 @@ class PayRequestPayload(BaseModel):
 
 @app.post("/pay")
 async def pay_route(request: Request, payload: PayRequestPayload):
+    idempotency_key = request.headers.get('idempotency-key')
+    if not idempotency_key:
+        return HTTPException(400, "Idempotency key required !")
+    db = db_session.SessionLocal()
     try:
-        db = db_session.SessionLocal()
-        idempotency_key = request.headers.get('idempotency-key')
-        if not idempotency_key:
-            return HTTPException(400, "Idempotency key required !")
-        
         payment_obj = db_session.Payment(
             idempotency_key=idempotency_key,
             status=db_session.Status.PROCESSING.value,
@@ -30,10 +30,36 @@ async def pay_route(request: Request, payload: PayRequestPayload):
         )
         db.add(payment_obj)
         db.commit()
-        return {"message": f"{idempotency_key}"}
+        await asyncio.sleep(2)
+        result = {
+            "payment_id": f"pay_{int(time.time())}",
+            "status": db_session.Status.SUCCESS.value, 
+            "message": "Payment Processed"
+        }
+        payment_obj.status = db_session.Status.SUCCESS.value
+        payment_obj.response = result
+        db.commit()
+        return result
     except IntegrityError as e:
         db.rollback()
-        return HTTPException(400, str(e))
+        existing = (
+            db.query(db_session.Payment)
+            .filter_by(idempotency_key=idempotency_key).first()
+        )
+        if existing.status == db_session.Status.SUCCESS.value:
+            return existing.response
+        elif existing.status == db_session.Status.FAILED.value:
+            return HTTPException(status_code=400, detail="Failed")
+        elif existing.status == db_session.Status.PROCESSING.value:
+            return HTTPException(status_code=409, detail="Processing")
+        return HTTPException(500, str(e))
     except Exception as e:
+        existing = (
+            db.query(db_session.Payment)
+            .filter_by(idempotency_key=idempotency_key).first()
+        )
+        existing.status = db_session.Status.FAILED.value
         return HTTPException(500, f"An error occurred while processing: {str(e)}")
+    finally:
+        db.close()
     
