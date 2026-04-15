48277494ee6843475ae1058d9f20eaac7d7f901c
Muteen Kundangar <kundangarmuteen@gmail.com>
2026-04-15 23:32:32 +0530

    Got payment route working with sqlalchemy

--- py-server/main.py
+++ py-server/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, Request, HTTPException
 from pydantic import BaseModel
-import db
+from sqlalchemy.exc import IntegrityError
+
+import db as db_session
 
 app = FastAPI()
 
@@ -16,10 +18,22 @@ class PayRequestPayload(BaseModel):
 @app.post("/pay")
 async def pay_route(request: Request, payload: PayRequestPayload):
     try:
+        db = db_session.SessionLocal()
         idempotency_key = request.headers.get('idempotency-key')
         if not idempotency_key:
             return HTTPException(400, "Idempotency key required !")
+        
+        payment_obj = db_session.Payment(
+            idempotency_key=idempotency_key,
+            status=db_session.Status.PROCESSING.value,
+            amount=payload.amount
+        )
+        db.add(payment_obj)
+        db.commit()
         return {"message": f"{idempotency_key}"}
+    except IntegrityError as e:
+        db.rollback()
+        return HTTPException(400, str(e))
     except Exception as e:
         return HTTPException(500, f"An error occurred while processing: {str(e)}")
     
